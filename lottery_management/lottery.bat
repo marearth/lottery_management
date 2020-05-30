0cb79a6f7bb0b74eda5fdc67148a010c4d79dead
marearth <niezhiwen.nie@gmail.com>
2020-05-30 17:15:04 +0800

    second commit

--- lottery_management/lottery.bat
+++
@@ -1,3 +0,0 @@
-START /B python E:\Code\python\lottery_management\scrape_webpage.py
-START /B python E:\Code\python\lottery_management\evaluate_lottery.py
-START /B python E:\Code\python\lottery_management\predict_lottery.py
