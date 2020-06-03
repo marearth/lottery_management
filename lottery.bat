ab84fbfcb2dd713ef4b3c41cb01bf518b8ed3159
marearth <niezhiwen.nie@gmail.com>
2020-06-03 16:23:14 +0800

    avoid asychronization

--- lottery.bat
+++ lottery.bat
@@ -1,3 +1,3 @@
-START /B python E:\Code\python\lottery_management\scrape_webpage.py
-START /B python E:\Code\python\lottery_management\evaluate_lottery.py
-START /B python E:\Code\python\lottery_management\predict_lottery.py
+START "lottery" /wait /B python E:\Code\python\lottery_management\scrape_webpage.py
+START "lottery" /wait /B python E:\Code\python\lottery_management\evaluate_lottery.py
+START "lottery" /wait /B python E:\Code\python\lottery_management\predict_lottery.py
