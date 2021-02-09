55caff10c18f0840b25a55eb2992de82287d10fe
marearth <niezhiwen.nie@gmail.com>
2021-02-09 10:59:09 +0800

    record execution information using info.txt

--- lottery.bat
+++ lottery.bat
@@ -1,3 +1,4 @@
-START "lottery" /wait /B python E:\Code\python\lottery_management\scrape_webpage.py
-START "lottery" /wait /B python E:\Code\python\lottery_management\evaluate_lottery.py
-START "lottery" /wait /B python E:\Code\python\lottery_management\predict_lottery.py
+cd /d E:\Code\python\lottery_management
+START "lottery" /wait /B python E:\Code\python\lottery_management\scrape_webpage.py > info.txt | type info.txt
+START "lottery" /wait /B python E:\Code\python\lottery_management\evaluate_lottery.py >> info.txt | type info.txt
+START "lottery" /wait /B python E:\Code\python\lottery_management\predict_lottery.py >> info.txt | type info.txt
